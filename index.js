105d8489aa4ebeb1dd57e670956538d74948bb08
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2022-02-11 13:52:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 document.body.style.backgroundColor = "blue";
+const $url = "https://injmed.wales.nhs.uk/?ID=38dcf4e35ed00c17c0f54cc9b7bb4033346";
+const iab = cordova.InAppBrowser.open($url, "_blank", "location=no,closebuttoncaption=Exit,lefttoright=yes,hidespinner=yes,toolbarposition=top");
+
