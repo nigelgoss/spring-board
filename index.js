e64f4bd48df176536439aadb5a9832b0071b473e
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2022-02-11 14:20:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ const loadURL = ($url) => {
 };
 
 let button = document.createElement("button");
+document.body.appendChild(button);
 button.textContent = "Medusa";
 button.onpointerdown = () => { loadURL("https://injmed.wales.nhs.uk/?ID=38dcf4e35ed00c17c0f54cc9b7bb4033346"); };
