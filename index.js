655d7a2e4d62e51a9ad613177ba7d362d4bc9082
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2022-02-11 14:18:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 document.body.style.backgroundColor = "pink";
 
-setTimeout(() => {
-const $url = "https://injmed.wales.nhs.uk/?ID=38dcf4e35ed00c17c0f54cc9b7bb4033346";
-const iab = cordova.InAppBrowser.open($url, "_blank", "location=no,closebuttoncaption=Exit,lefttoright=yes,hidespinner=yes,toolbarposition=top");
-}, 5000);
+let iab;
+
+const loadURL = ($url) => {
+  iab = cordova.InAppBrowser.open($url, "_blank", "cleardata=yes,location=no,closebuttoncaption=Exit,lefttoright=yes,hidespinner=yes,toolbarposition=top,navigationbuttoncolor=#FFFFFF,closebuttoncolor=#FFFFFF,toolbarcolor=#005EB8");  
+  setTimeout(() => { iab.close(); }, 15 * 1000);
+};
+
+let button = document.createElement("button");
+button.textContent = "Medusa";
+button.onpointerdown = () => { $url(""); };
